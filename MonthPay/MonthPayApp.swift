9aa138b8299f2f02d62d8a58cceea31d34b88112
oogioogi <detacl73@gmail.com>
2021-08-13 19:28:23 +0900

    Initial Commit

---
+++ MonthPay/MonthPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthPayApp.swift
+//  MonthPay
+//
+//  Created by 이용석 on 2021/08/13.
+//
+
+import SwiftUI
+
+@main
+struct MonthPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
